3141a000ffb26b0fe39a3e96c0cf06b6431ac878
Tuanminh2k8 <tuanminh00002008@gmail.com>
2026-06-10 14:27:41 +0700

    test

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace KIEMTHUCOBAN
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
